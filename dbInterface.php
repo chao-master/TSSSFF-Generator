7fb18708f26b59917ff139eb1856e1d717b13bfe
chao-master <ripppony@gmail.com>
2015-07-01 01:52:39 +0100

    debugging

--- dbInterface.php
+++ dbInterface.php
@@ -216,10 +216,13 @@
         if (amount !== null){
             if ($mode == "view"){
                 if ($setViewKey !== null){
+                    print "\\setView $viewKey,$amount,"view",$setViewKey";
                     print json_encode(getSet($viewKey,$amount,"view",$setViewKey));
                 } else if ($setEditKey !== null){
+                    print "\\getView $viewKey,$amount,"edit",$setEditKey"
                     print json_encode(getSet($viewKey,$amount,"edit",$setEditKey));
                 } else {
+                    print "\\getRange $viewKey,$amount,$filter"
                     print json_encode(getRange($viewKey,$amount,$filter));
                 }
             } else {
