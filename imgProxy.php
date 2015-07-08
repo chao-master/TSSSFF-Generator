cf29e17f8f9f027276db640c13f2112adaca8410
chao-master <ripppony@gmail.com>
2015-07-08 19:38:13 +0100

    Adding image resizing into the imgProxy

--- imgProxy.php
+++ imgProxy.php
@@ -2,25 +2,24 @@
 $remoteImage = $_GET["img"];
 
 $allowedSites = [
-    "https://derpicdn.net" => function($url){
-        if (!preg_match("~https://derpicdn.net/img/view/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)[^.]*(\..*)*~",$url,$matches)){
-            return $url;
-        } else {
-            return "https://derpicdn.net/img/$matches[1]/$matches[2]/$matches[3]/$matches[4]/medium$matches[5]";
-        }
-    }
+    "https://derpicdn.net",
+    "http://derpicdn.net"
 ];
 
-foreach ($allowedSites as $site => $mapping) {
+foreach ($allowedSites as $site) {
     //Check if it's on the allowed site's list
     if (substr($remoteImage, 0, strlen($site)) === $site){
-        if ($mapping){
-            $remoteImage = $mapping($remoteImage);
-        }
+
+        $handle = fopen($remoteImage, 'rb');
+        $img = new Imagick();
+        $img->readImageFile($handle);
+        $img->resizeImage(601, 444, 0, 0);
+
         $imginfo = getimagesize($remoteImage);
         header("Content-type: ".$imginfo['mime']);
-        readfile($remoteImage);
+        echo $img;
         die();
+
     }
 }
 //If not try a redirect to see if they have CROS
